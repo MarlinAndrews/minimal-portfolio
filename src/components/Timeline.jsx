1edd5795896b0a95dcd006f43df53ea43879fdc8
Marlin Andrews <marlindandrews@gmail.com>
2024-03-21 14:01:08 +1100

    Add timeline components and functionality

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import timeline from'../data/timeline';
+import TimelineItem from './TimelineItem';
 
 function Timeline() {
     return (
-        <div></div>
+        <div className='flex flex-col md:flex-row justify-center my-20'>
+            <div className='w-full md:w-7/12'>
+            {timeline.map(item => (
+                <TimelineItem
+                year={item.year}
+                title={item.title}
+                details={(item.details)}
+                />
+            ))}
+        </div>
+       </div> 
     )
 }
 
---
+++ src/data/timeline.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Freelance Web Developer',
+        details: ' I am currently working as a freelance web developer, as well as looking for opportunities for collaboration ',
+
+    },
+    {
+        year: 'Today',
+        title: 'The Odin Project student', 
+        details: 'Continuing to further my knowledge in Full-Stack web development, I am currently a student at The Odin Project, a free open source web development bootcamp.',
+        
+    },
+    {
+        year: '2023',
+        title: 'Certificate of Responsive Web Development - FreeCodeCamp',
+        details: 'Completed the Responsive Web Development course on FreeCodeCamp.',
+        
+    },
+    {
+        year: '2023',
+        title: 'Diploma of Applied Information Technology - La Trobe Universtiy',
+        details: 'Graduated from La Trobe University with a Diploma of Applied Information Technology.',
+        
+    },
+    {
+        year: '2023',
+        title: 'High-Distinction in Java Programming and Programming environments - La Trobe University',
+        details: 'Completed units in development environments, Java programming and object-oriented programming ',
+        
+    },
+    {
+        year: '2018',
+        title: 'Wrote my first line of code',
+        details: 'Made the realisation that there was no greater feeling that breaking down a problem and solving it with code having dabbled with swift and harvard cs50.',
+    },
+]
